c9ebc6c25398d680ce2e8f2e2ca64e4c183d1346
vitaliknbest <dmitriydvoranov@gmail.com>
2017-02-15 20:09:13 +0200

    first commit

--- lab 6/task 11.py
+++ lab 6/task 11.py
@@ -4,14 +4,26 @@ class date:
     days = range(1, 32)
     month = range(1, 13)
     years = range(1901, 2020)
-
-d,m,y = int(input('day :')), int(input('month :')), int(input('year :'))
-if d in range(1,32) and m in range(1,13) and y in range(1901,2020):
-    if d + 1 not in date.days and m + 1 in date.month:
-        print('1', '.', m+1, '.', y)
-    elif d in date.days :
-        print(d +1, '.', m, '.', y)
-    else:
-        print('1', '.', '1',  '.', y +1)
-else :
-    print("Введіть коректні дані")
+while True:
+    try:
+        d,m,y = int(input('day :')),int(input('month :')),int(input('year :'))
+        if d in range(1,32) and m in range(1,13) and y in range(1901,2020):
+            if d + 1 not in date.days and m + 1 in date.month:
+                print('1', '.', m+1, '.', y)
+            elif d + 1 in date.days :
+                print(d +1, '.', m, '.', y)
+            else:
+                print('1', '.', '1',  '.', y +1)
+        else :
+            print("Введіть коректні дані")
+    except ValueError:
+        print('Введіть коректні дані')
+    while True:
+        do = input("Бажаєте продовжити ?(y /n )\n")
+        if do == "y" or do == "n":
+            break
+        print('Не коректні данні!')
+    if do == "y":
+        continue
+    if do == "n":
+        break
--- lab 6/task12.py
+++ lab 6/task12.py
@@ -9,20 +9,34 @@ class measure(Enum):
     metre = 3
     millimetre = 4
     centimetre = 5
-    x = float(input('leght:'))
-    p = [input('measure:')]
-    if p == measure(1):
-        x *= 0.1
-    elif p == measure(2):
-        x *= 1000
-    elif p == measure(3):
-        x *= 1
-    elif p == measure(4):
-        x *= 0.001
-    elif p == measure(5):
-        x *= 0.01
-    else:
-        print("введіть коректні дані")
-print(x)
-    
-    
+while True:
+    try:
+        print("measure:decimetre, kilometre, metre, millimetre, centimetre  ")
+        x = float(input('leght:'))
+        p = measure[input('measure:')]
+        if p == measure(1):
+            x /= 10
+        elif p == measure(2):
+            x *= 1000
+        elif p == measure(3):
+            x *= 1
+        elif p == measure(4):
+            x /=1000 
+        elif p == measure(5):
+            x /= 100
+        else:
+            print("введіть коректні дані")   
+        print( x)    
+        while True:
+            do = input("Бажаєте продовжити ?(y /n )\n")
+            if do == "y" or do == "n":
+                break
+            print('Не коректні данні!')
+        if do == "y":
+            continue
+        if do == "n":
+            break
+    except ValueError:
+        print("Введіть правельне значення")
+
+        
