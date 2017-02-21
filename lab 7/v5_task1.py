5794b69a8a5d15da80f3e4dc5cd719b328debe1e
vitaliknbest <dmitriydvoranov@gmail.com>
2017-02-21 20:29:38 +0200

    2 commit not done

--- lab 7/v5_task1.py
+++ lab 7/v5_task1.py
@@ -1,19 +1,26 @@
+#  Насадюк Віталій
+#  Вывести кол-во чисел, которые входят в последовательность боле одного раза
 while True:
     try:
-        s = input("введіть послідовність цілих чисел через кому\n")
+        s = input("введіть послідовність цілих чисел через кому\n").split(',')
+        se = set()
         for i in s:
-            if i == ',' or int(i)== 0-9:
-                s = str(s)
-                s = s.split(',')
-        print(len(set(s)))
+            if i.isdigit() and s.count(i) > 1:            
+                se.add(i)
+            else:
+                raise ValueError
+        print(len(se))
     except ValueError:
         print('Введіть числа через кому')
     while True:
         do = input("Бажаєте продовжити ?(y /n )\n")
         if do == "y" or do == "n":
             break
-        print('Не коректні данні!')
-        if do == "y":
-            continue
-        if do == "n":
-            break
+        else:
+
+            print('Не коректні данні!')
+    if do == "y":
+        continue
+    if do == "n":
+        break
+        
--- lab 7/v5_task2.py
+++ lab 7/v5_task2.py
@@ -1,7 +1,31 @@
-k = set('бвгджзйклмнпрстфхцчшщ')
-s = input('Ведіть слова ( через кому ) а в кінці крапку')
-sett = set()
-for i in s:
-    if s.count(i) == 1 and k & set(i) != set() :
-        sett.add(i)
-print(sorted(sett))
+# Насадюк Віталій
+#Дана непустая последовательность слов из строчных руссских букв; между соседними словами -
+# - запятая, за последним словом - точка. Вывести на экран в алфавитном порядке все звонкие
+# согласные буквы, которые входят только в одно слово.
+
+while True:
+    try:
+        buk = set('бвгджзйклмнпрстфхцчшщ')
+        s = input('Ведіть слова ( через кому ) а в кінці крапку\n')
+        spusok = set()
+        if s.endswith('.'):
+            s = s.split(',')
+            for i in buk:
+                for j in s:
+                 if j.count(i) != 0 and str(list(s)-list(j)).count(i)==0 :
+                     spusok.add(i)
+            print(sorted(spusok))
+        else :
+            raise ValueError
+        
+    except ValueError:
+        print("Ведіть слова російськими буквами через кому а за останнім крапку\n")
+    while True:
+        do = input("Бажаєте продовжити ?(y /n )\n")
+        if do == "y" or do == "n":
+            break
+        print('Не коректні данні!')
+        if do == "y":
+            continue
+        if do == "n":
+            break
