9784f1275db18a31a8190c6ea7402815aa733208
vitaliknbest <dmitriydvoranov@gmail.com>
2017-02-22 21:23:03 +0200

    done

--- lab 7/v5_task2.py
+++ lab 7/v5_task2.py
@@ -2,19 +2,33 @@
 #Дана непустая последовательность слов из строчных руссских букв; между соседними словами -
 # - запятая, за последним словом - точка. Вывести на экран в алфавитном порядке все звонкие
 # согласные буквы, которые входят только в одно слово.
+def isIntersection(word1,word2):
+    return len(word1 & word2) !=0
+
+def intersection(word1,word2):
+    return word1 & word2
+
 
 while True:
     try:
         buk = set('бвгджзйклмнпрстфхцчшщ')
         s = input('Ведіть слова ( через кому ) а в кінці крапку\n')
         spusok = set()
+        listt = []
         if s.endswith('.'):
             s = s.split(',')
-            for i in buk:
-                for j in s:
-                 if j.count(i) != 0 and str(list(s)-list(j)).count(i)==0 :
-                     spusok.add(i)
-            print(sorted(spusok))
+            n = len(s)
+            for i in s:
+                listt.append(intersection(buk, set(i)))
+            hasIntersction = [False] * n
+            for i in range(len(listt) - 1):
+                for  j in range(i + 1, len(listt)):
+                    if intersection(listt[i], listt[j]):
+                        hasIntersction[i]=True
+                        hasIntersction[j]=True
+            for i in range(len(listt)):
+                if(not(hasIntersction[i])):
+                    print(sorted(listt[i]))
         else :
             raise ValueError
         
@@ -24,8 +38,9 @@ while True:
         do = input("Бажаєте продовжити ?(y /n )\n")
         if do == "y" or do == "n":
             break
-        print('Не коректні данні!')
-        if do == "y":
-            continue
-        if do == "n":
-            break
+        else :
+            print('Не коректні данні!')
+    if do == "y":
+        continue
+    if do == "n":
+        break
